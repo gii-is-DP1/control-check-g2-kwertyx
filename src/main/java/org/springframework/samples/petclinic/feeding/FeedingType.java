c2e1af8774347fb4ed34811b29ad55dc8dd6226a
Daniel Díaz <daniel0diiaz@gmail.com>
2022-01-14 12:23:59 +0100

    Error en Test 2 y test 8 no hecho

--- src/main/java/org/springframework/samples/petclinic/feeding/FeedingType.java
+++ src/main/java/org/springframework/samples/petclinic/feeding/FeedingType.java
@@ -1,5 +1,14 @@
 package org.springframework.samples.petclinic.feeding;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+import org.springframework.samples.petclinic.model.BaseEntity;
 import org.springframework.samples.petclinic.pet.PetType;
 
 import lombok.Getter;
@@ -7,9 +16,19 @@ import lombok.Setter;
 
 @Getter
 @Setter
-public class FeedingType {
+@Entity
+@Table(name = "feedingtypes")
+public class FeedingType extends BaseEntity {
     Integer id;
+
+    @Size(min=5, max=30)
+    @Column(unique = true)
     String name;
+
+    @NotEmpty
     String description;
+
+    @ManyToOne
+    @NotEmpty
     PetType petType;
 }
--- src/main/java/org/springframework/samples/petclinic/feeding/FeedingTypeFormatter.java
+++ src/main/java/org/springframework/samples/petclinic/feeding/FeedingTypeFormatter.java
@@ -3,22 +3,33 @@ package org.springframework.samples.petclinic.feeding;
 import java.text.ParseException;
 import java.util.Locale;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.format.Formatter;
 import org.springframework.stereotype.Component;
 
 @Component
 public class FeedingTypeFormatter implements Formatter<FeedingType>{
 
+    @Autowired
+    private FeedingService fs;
+
     @Override
     public String print(FeedingType object, Locale locale) {
-        // TODO Auto-generated method stub
+        if(object != null) {
+            return String.format(locale, "%s", object.getName());
+        }
         return null;
     }
 
     @Override
     public FeedingType parse(String text, Locale locale) throws ParseException {
-        // TODO Auto-generated method stub
-        return null;
+        FeedingType ft = this.fs.getFeedingType(text);
+
+        if(ft!=null) {
+            return ft;
+        } else {
+            throw new ParseException("Hola", 1);
+        }
     }
     
 }
